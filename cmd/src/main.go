3e10b7c94ff3fdd24bc91df0a02ad347ec05938c
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-03-16 03:19:57 -0600

    agregado cuadraticas

--- cmd/src/main.go
+++ cmd/src/main.go
@@ -16,6 +16,15 @@ func main() {
 	fmt.Println(f(0))
 	fmt.Println(f(1))
 	fmt.Println(f(2))
+	f2, err := cuadratica([]float64{-2, -1, 0, 1, 2}, []float64{9.7, 5.2, 3.4, 4.5, 8.5})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(f2(-2))
+	fmt.Println(f2(-1))
+	fmt.Println(f2(0))
+	fmt.Println(f2(1))
+	fmt.Println(f2(2))
 }
 
 type MatrizExtendida struct {
@@ -139,4 +148,41 @@ func cubica(x []float64, y []float64) (func(float64) float64, error) {
 	return func(x float64) float64 {
 		return coeficientes[0]*math.Pow(x, 3) + coeficientes[1]*math.Pow(x, 2) + coeficientes[2]*x + coeficientes[3]
 	}, nil
+}
+
+func cuadratica(x []float64, y []float64) (func(float64) float64, error) {
+	if len(x) != len(y) {
+		return func(x float64) float64 { return 0.0 }, errors.New("lengths don't match")
+	}
+	sums := [7]float64{}
+	fmt.Printf("x\ty\tx2\tx3\tx4\txy\tx2y\n")
+	for i := range x {
+		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n", x[i], y[i], math.Pow(x[i], 2), math.Pow(x[i], 3), math.Pow(x[i], 4), x[i]*y[i], math.Pow(x[i], 2)*y[i])
+		sums[0] += x[i]
+		sums[1] += y[i]
+		sums[2] += math.Pow(x[i], 2)
+		sums[3] += math.Pow(x[i], 3)
+		sums[4] += math.Pow(x[i], 4)
+		sums[5] += x[i] * y[i]
+		sums[6] += math.Pow(x[i], 2) * y[i]
+	}
+	fmt.Println("sumas:")
+	for _, v := range sums {
+		fmt.Printf("%v\t", v)
+	}
+	fmt.Println()
+	m := new(MatrizExtendida)
+	m.ecuaciones = [][]float64{
+		{sums[2], sums[0], float64(len(x))},
+		{sums[3], sums[2], sums[0]},
+		{sums[4], sums[3], sums[2]},
+	}
+	m.igualdades = []float64{
+		sums[1], sums[5], sums[6],
+	}
+	m.Organizar()
+	fmt.Println()
+	m.Mostrar()
+	coeficientes := m.Montante()
+	return func(x float64) float64 { return coeficientes[0]*math.Pow(x, 2) + coeficientes[1]*x + coeficientes[2] }, nil
 }

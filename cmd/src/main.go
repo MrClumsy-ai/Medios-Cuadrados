22fc9eb36426a46931bb3c089b25754919ee2480
MrClumsy-ai <eduardomenchaca01@hotmail.com>
2025-03-23 14:08:37 -0600

    feat: added lineal con funcion

--- cmd/src/main.go
+++ cmd/src/main.go
@@ -7,7 +7,43 @@ import (
 )
 
 func main() {
-	f, err := cubica([]float64{-2, -1, 0, 1, 2}, []float64{3, 0, 2, 4, 4})
+	/* ejercicio1 := MatrizExtendida{
+		Ecuaciones: [][]float64{
+			{4.0, 14.3, -2.0514748815},
+			{14.3, 59.45, -6.4484474547},
+			{-2.0514748815, -6.4484474547, 1.5974511381},
+		},
+		Igualdades: []float64{14.1, 56.03, -6.8842498511},
+	}
+	ejercicio1.Mostrar()
+	results := ejercicio1.Montante()
+	for i := range results {
+		fmt.Println(results[i])
+	}
+	ejercicio2 := MatrizExtendida{
+		Ecuaciones: [][]float64{
+			{4.0, 14.3, 59.45, -2.0514748815},
+			{14.3, 59.45, 271.883, -6.4484474547},
+			{59.45, 271.883, 1308.2129, -23.8850981208},
+			{-2.0514748815, -6.4484474547, -23.8850981208, 1.597451138},
+		},
+		Igualdades: []float64{14.1, 56.03, 250.433, -6.8842498511},
+	}
+	ejercicio2.Mostrar()
+	results = ejercicio2.Montante()
+	for i := range results {
+		fmt.Println(results[i])
+	}
+	*/
+	results, err := LinealConFuncion([]float64{1.9, 2.4, 4.8, 5.2}, []float64{2.5, 2.7, 3.7, 5.2}, func(x float64) float64 { return 1 / math.Tan(x) })
+	if err != nil {
+		panic(err)
+	}
+	for i := range results {
+		fmt.Println(results[i])
+	}
+
+	/* f, err := cubica([]float64{-2, -1, 0, 1, 2}, []float64{3, 0, 2, 4, 4})
 	if err != nil {
 		panic(err)
 	}
@@ -16,7 +52,7 @@ func main() {
 	fmt.Println(f(0))
 	fmt.Println(f(1))
 	fmt.Println(f(2))
-	f2, err := cuadratica([]float64{-2, -1, 0, 1, 2}, []float64{9.7, 5.2, 3.4, 4.5, 8.5})
+	f2, err := cuadratica([]float64{-2, -1, 0, 1, 2}, []float64{12.7, 3.2, -3.3, -6.8, -7.3})
 	if err != nil {
 		panic(err)
 	}
@@ -24,43 +60,43 @@ func main() {
 	fmt.Println(f2(-1))
 	fmt.Println(f2(0))
 	fmt.Println(f2(1))
-	fmt.Println(f2(2))
+	fmt.Println(f2(2)) */
 }
 
 type MatrizExtendida struct {
-	ecuaciones [][]float64
-	igualdades []float64
+	Ecuaciones [][]float64
+	Igualdades []float64
 }
 
 func (m MatrizExtendida) Mostrar() {
 	charr := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	for i := range len(m.ecuaciones) {
-		for j := range len(m.ecuaciones) {
-			fmt.Printf("%v%v", m.ecuaciones[i][j], charr[j%26])
-			if j < len(m.ecuaciones)-1 {
+	for i := range len(m.Ecuaciones) {
+		for j := range len(m.Ecuaciones) {
+			fmt.Printf("%v%v", m.Ecuaciones[i][j], charr[j%26])
+			if j < len(m.Ecuaciones)-1 {
 				fmt.Print(" + ")
 			}
 		}
-		fmt.Printf(" = %v\n", m.igualdades[i])
+		fmt.Printf(" = %v\n", m.Igualdades[i])
 	}
 }
 
 func (m MatrizExtendida) Organizar() {
-	for i := range len(m.ecuaciones) {
-		if m.ecuaciones[i][i] != 0 {
+	for i := range len(m.Ecuaciones) {
+		if m.Ecuaciones[i][i] != 0 {
 			continue
 		}
-		for j := range len(m.ecuaciones) {
+		for j := range len(m.Ecuaciones) {
 			if i == j {
 				continue
 			}
-			if m.ecuaciones[j][i] != 0 {
-				temp := m.ecuaciones[i]
-				m.ecuaciones[i] = m.ecuaciones[j]
-				m.ecuaciones[j] = temp
-				temp2 := m.igualdades[i]
-				m.igualdades[i] = m.igualdades[j]
-				m.igualdades[j] = temp2
+			if m.Ecuaciones[j][i] != 0 {
+				temp := m.Ecuaciones[i]
+				m.Ecuaciones[i] = m.Ecuaciones[j]
+				m.Ecuaciones[j] = temp
+				temp2 := m.Igualdades[i]
+				m.Igualdades[i] = m.Igualdades[j]
+				m.Igualdades[j] = temp2
 				break
 			}
 		}
@@ -69,47 +105,47 @@ func (m MatrizExtendida) Organizar() {
 
 func (m MatrizExtendida) Montante() []float64 {
 	pivote := 1.0
-	prev_ecuaciones := make([][]float64, len(m.ecuaciones))
-	prev_igualdades := make([]float64, len(m.ecuaciones))
-	for i := range len(m.ecuaciones) {
+	prev_ecuaciones := make([][]float64, len(m.Ecuaciones))
+	prev_igualdades := make([]float64, len(m.Ecuaciones))
+	for i := range len(m.Ecuaciones) {
 		fmt.Println()
-		for j := range len(m.ecuaciones) {
-			prev_ecuaciones[j] = make([]float64, len(m.ecuaciones[j]))
-			prev_igualdades[j] = m.igualdades[j]
-			for k := range len(m.ecuaciones[j]) {
-				prev_ecuaciones[j][k] = m.ecuaciones[j][k]
+		for j := range len(m.Ecuaciones) {
+			prev_ecuaciones[j] = make([]float64, len(m.Ecuaciones[j]))
+			prev_igualdades[j] = m.Igualdades[j]
+			for k := range len(m.Ecuaciones[j]) {
+				prev_ecuaciones[j][k] = m.Ecuaciones[j][k]
 			}
 		}
-		for row := range len(m.ecuaciones) {
-			for col := range len(m.ecuaciones) + 1 {
+		for row := range len(m.Ecuaciones) {
+			for col := range len(m.Ecuaciones) + 1 {
 				if row == col && row == i || row == i {
 					continue
 				}
 				if col == i {
-					m.ecuaciones[row][col] = 0
+					m.Ecuaciones[row][col] = 0
 					continue
 				}
-				if col < len(m.ecuaciones) {
-					m.ecuaciones[row][col] = (prev_ecuaciones[i][i]*prev_ecuaciones[row][col] - prev_ecuaciones[row][i]*prev_ecuaciones[i][col]) / pivote
+				if col < len(m.Ecuaciones) {
+					m.Ecuaciones[row][col] = (prev_ecuaciones[i][i]*prev_ecuaciones[row][col] - prev_ecuaciones[row][i]*prev_ecuaciones[i][col]) / pivote
 				} else {
-					m.igualdades[row] = (prev_ecuaciones[i][i]*prev_igualdades[row] - prev_ecuaciones[row][i]*prev_igualdades[i]) / pivote
+					m.Igualdades[row] = (prev_ecuaciones[i][i]*prev_igualdades[row] - prev_ecuaciones[row][i]*prev_igualdades[i]) / pivote
 				}
 			}
 		}
 		fmt.Printf("iteracion: %v\npivote: %v\n", i+1, pivote)
-		pivote = m.ecuaciones[i][i]
+		pivote = m.Ecuaciones[i][i]
 		m.Mostrar()
 	}
-	results := make([]float64, len(m.ecuaciones))
-	for i := range m.ecuaciones {
-		results[i] = m.igualdades[i] / pivote
+	results := make([]float64, len(m.Ecuaciones))
+	for i := range m.Ecuaciones {
+		results[i] = m.Igualdades[i] / pivote
 	}
 	return results
 }
 
 func cubica(x []float64, y []float64) (func(float64) float64, error) {
 	if len(x) != len(y) {
-		return func(x float64) float64 { return 0.0 }, errors.New("lengths don't match")
+		return nil, errors.New("lengths don't match")
 	}
 	sums := [10]float64{}
 	fmt.Printf("x\ty\tx2\tx3\tx4\tx5\tx6\txy\tx2y\tx3y\n")
@@ -132,32 +168,34 @@ func cubica(x []float64, y []float64) (func(float64) float64, error) {
 	}
 	fmt.Println()
 	m := new(MatrizExtendida)
-	m.ecuaciones = [][]float64{
+	m.Ecuaciones = [][]float64{
 		{sums[3], sums[2], sums[0], float64(len(x))},
 		{sums[4], sums[3], sums[2], sums[0]},
 		{sums[5], sums[4], sums[3], sums[2]},
 		{sums[6], sums[5], sums[4], sums[3]},
 	}
-	m.igualdades = []float64{
+	m.Igualdades = []float64{
 		sums[1], sums[7], sums[8], sums[9],
 	}
 	m.Organizar()
 	fmt.Println()
 	m.Mostrar()
 	coeficientes := m.Montante()
+	fmt.Println(coeficientes)
 	return func(x float64) float64 {
 		return coeficientes[0]*math.Pow(x, 3) + coeficientes[1]*math.Pow(x, 2) + coeficientes[2]*x + coeficientes[3]
 	}, nil
 }
 
 func cuadratica(x []float64, y []float64) (func(float64) float64, error) {
+	println("CUADRATICAS")
 	if len(x) != len(y) {
 		return func(x float64) float64 { return 0.0 }, errors.New("lengths don't match")
 	}
 	sums := [7]float64{}
 	fmt.Printf("x\ty\tx2\tx3\tx4\txy\tx2y\n")
 	for i := range x {
-		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n", x[i], y[i], math.Pow(x[i], 2), math.Pow(x[i], 3), math.Pow(x[i], 4), x[i]*y[i], math.Pow(x[i], 2)*y[i])
+		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\n", x[i], y[i], math.Pow(x[i], 2), math.Pow(x[i], 3), math.Pow(x[i], 4), x[i]*y[i], math.Pow(x[i], 2)*y[i])
 		sums[0] += x[i]
 		sums[1] += y[i]
 		sums[2] += math.Pow(x[i], 2)
@@ -172,17 +210,57 @@ func cuadratica(x []float64, y []float64) (func(float64) float64, error) {
 	}
 	fmt.Println()
 	m := new(MatrizExtendida)
-	m.ecuaciones = [][]float64{
+	m.Ecuaciones = [][]float64{
 		{sums[2], sums[0], float64(len(x))},
 		{sums[3], sums[2], sums[0]},
 		{sums[4], sums[3], sums[2]},
 	}
-	m.igualdades = []float64{
+	m.Igualdades = []float64{
 		sums[1], sums[5], sums[6],
 	}
+	m.Mostrar()
 	m.Organizar()
 	fmt.Println()
 	m.Mostrar()
 	coeficientes := m.Montante()
+	fmt.Println(coeficientes)
 	return func(x float64) float64 { return coeficientes[0]*math.Pow(x, 2) + coeficientes[1]*x + coeficientes[2] }, nil
+}
+
+// g(x) = a + bx + cf(x)
+func LinealConFuncion(x []float64, y []float64, f func(float64) float64) ([]float64, error) {
+	if len(x) != len(y) {
+		return nil, errors.New("lengths dont match")
+	}
+	sums := [8]float64{}
+	fmt.Printf("x\ty\tx2\tf(x)\txf(x)\tf(x)2\txy\tyf(x)")
+	for i := range x {
+		fmt.Printf("%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\t%v\n", x[i], y[i], math.Pow(x[i], 2), f(x[i]), x[i]*f(x[i]), math.Pow(f(x[i]), 2), x[i]*y[i], y[i]*f(x[i]))
+		sums[0] += x[i]
+		sums[1] += y[i]
+		sums[2] += math.Pow(x[i], 2)
+		sums[3] += f(x[i])
+		sums[4] += x[i] * f(x[i])
+		sums[5] += math.Pow(f(x[i]), 2)
+		sums[6] += x[i] * y[i]
+		sums[7] += y[i] * f(x[i])
+	}
+	fmt.Println("sumas:")
+	for _, v := range sums {
+		fmt.Printf("%v\t", v)
+	}
+	fmt.Println()
+	m := new(MatrizExtendida)
+	m.Ecuaciones = [][]float64{
+		{float64(len(x)), sums[0], sums[3]},
+		{sums[0], sums[2], sums[4]},
+		{sums[3], sums[4], sums[5]},
+	}
+	m.Igualdades = []float64{
+		sums[1], sums[6], sums[7],
+	}
+	m.Organizar()
+	fmt.Println()
+	m.Mostrar()
+	return m.Montante(), nil
 }
